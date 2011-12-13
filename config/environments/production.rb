8891d25a72f98e220c78cbddd821d711c124d07d
rubyshoes <hlogoma@hotmail.com>
2011-12-13 09:43:01 -0600

    revised the config/environments/production.rb to include do custom.css end

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -44,7 +44,9 @@ Tracking::Application.configure do
 
    Precompile additional assets (application.js, application.css,
    custom.css and all non-JS/CSS are already added)
-   config.assets.precompile += %w( search.js )
+   config.assets.precompile += %w( search.js ) do
+     custom.css
+  end
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
