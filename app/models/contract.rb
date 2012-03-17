59ede60d4807d996abe257d86fbf4941408d2dc4
rubyshoes <hlogoma@hotmail.com>
2012-03-17 14:26:54 -0500

    fixed the _fileds.html.haml file...it had extra = br/, and fixed codename to code_name in the code.rb model for both test and development

--- app/models/contract.rb
+++ app/models/contract.rb
@@ -18,10 +18,10 @@ class Contract < ActiveRecord::Base
   has_many :codes, :through => :codelines
 
   accepts_nested_attributes_for :clients
-  accepts_nested_attributes_for :codelines#, :reject_if => lambda { |a| a[:units_alloc].blank? }
-  accepts_nested_attributes_for :codes#, :reject_if => lambda { |a| a[:code_name].blank?
-                                                                   #a[:status].blank?
-                                                                   #a[:description].blank? }
+  accepts_nested_attributes_for :codelines, :reject_if => lambda { |a| a[:units_alloc].blank? }
+  accepts_nested_attributes_for :codes, :reject_if => lambda { |a| a[:code_name].blank?
+                                                                   a[:status].blank?
+                                                                   a[:description].blank? }
 
   attr_accessible :clients_attributes, :codes_attributes, :codelines, :codelines_attributes, :clients, :contract, :contract_attributes, :code, :code_attributes, :client, :client_attributes, :codes, :authnum, :st_date, :end_date
 
