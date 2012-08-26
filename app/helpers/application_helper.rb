c9bf16bb796b751a1c22de8c44c0047c16b8b94e
rubyshoes <hlogoma@hotmail.com>
2012-08-25 19:17:51 -0500

    changed the link_to_add_field to its work-around

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -16,11 +16,14 @@ module ApplicationHelper
 
   def link_to_add_fields(name, f, association)
     /name is the name of the link, f is the form builder, lastly we have the name of the association/
-    new_object = f.object.send(association).klass.new
-    id = new_object.object_id
-    fields = f.fields_for(association, new_object, child_index: id) do |builder|
+    /new_object = f.object.send(association).klass.new/
+    new_object = f.object.class.reflect_on_association(association).klass.new
+    fields = f.fields_for(association, new_object, :child_index => "new_#{association}") do |builder|
+    /id = new_object.object_id/
+    /fields = f.fields_for(association, new_object, child_index: id) do |builder|/
       render(association.to_s.singularize + "_fields", f: builder)
     end 
-    link_to(name, '#', class: "add_fields", data: {id: id, fields: fields.gsub("\n", "")})
+    /link_to(name, '#', class: "add_fields", data: {id: id, fields: fields.gsub("\n", "")})/
+    link_to_function(name, h("add_fields(this, \"#{association}\", \"#{escape_javascript(fields)}\")"))
   end
 end
